10d463faeaa1b2691ecd836233c3f127e6f9522c
Chris Recalis <crecalis@gmail.com>
2015-12-21 09:15:37 -0500

    a

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var seneca = require("seneca")();
 
+console.log("Starting seneca");
 seneca.add("cmd:add", function(msg, respond){
 	var sum = msg.left + msg.right;
 	respond(null, {answer: sum});
 });
 
 seneca.listen();
+console.log("started");
